ae9e44a8d63e7bc95d8f0ed4fd97160806c313c6
Ricardo Peres <rjperes@hotmail.com>
2024-09-20 11:34:02 +0000

    SoftDeleteConvention is now generic and configurable

--- Conventions/SoftDeleteConvention.cs
+++ Conventions/SoftDeleteConvention.cs
@@ -6,25 +6,28 @@ using System.Reflection;
 
 namespace EFSoftDeletes.Conventions
 {
-    internal class SoftDeleteConvention : IModelFinalizingConvention
+    internal class SoftDeleteConvention<T> : IModelFinalizingConvention
     {
-        private const string _isDeletedProperty = Constants.IsDeletedProperty;
-        private static readonly MethodInfo _propertyMethod = typeof(EF).GetMethod(nameof(EF.Property), BindingFlags.Static | BindingFlags.Public)!.MakeGenericMethod(typeof(bool));
+        private const string _isDeletedProperty = "IsDeleted";
+        private static readonly MethodInfo _propertyMethod = typeof(EF).GetMethod(nameof(EF.Property), BindingFlags.Static | BindingFlags.Public)!.MakeGenericMethod(typeof(T));
 
-        private static LambdaExpression GetIsDeletedRestriction(Type type)
+        public T DeletedValue { get; init; } = default!;
+        public string IsDeletedProperty { get; init; } = _isDeletedProperty;
+    
+        private LambdaExpression GetIsDeletedRestriction(Type type)
         {
             var parm = Expression.Parameter(type, "it");
-            var prop = Expression.Call(_propertyMethod, parm, Expression.Constant(_isDeletedProperty));
-            var condition = Expression.MakeBinary(ExpressionType.Equal, prop, Expression.Constant(false));
+            var prop = Expression.Call(_propertyMethod, parm, Expression.Constant(IsDeletedProperty));
+            var condition = Expression.MakeBinary(ExpressionType.Equal, prop, Expression.Constant(DeletedValue));
             var lambda = Expression.Lambda(condition, parm);
             return lambda;
         }
-
+    
         public void ProcessModelFinalizing(IConventionModelBuilder modelBuilder, IConventionContext<IConventionModelBuilder> context)
         {
-            foreach (var entityType in modelBuilder.Metadata.GetEntityTypes().Where(entityType => typeof(ISoftDeletable).IsAssignableFrom(entityType.ClrType)))
+            foreach (var entityType in modelBuilder.Metadata.GetEntityTypes().Where(entityType => typeof(ISoftDeletable<T>).IsAssignableFrom(entityType.ClrType)))
             {
-                entityType.AddProperty(_isDeletedProperty, typeof(bool));
+                entityType.AddProperty(IsDeletedProperty, typeof(T));
                 modelBuilder.Entity(entityType.ClrType)!.HasQueryFilter(GetIsDeletedRestriction(entityType.ClrType));
             }
         }
